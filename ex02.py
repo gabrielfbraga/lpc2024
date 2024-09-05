d178df4d83c8f03aa23cc43528ed879531c37e4a
gabrielfbraga <gfbr.snf24@uea.edu.br>
2024-09-04 23:38:35 -0400

    Delete ex02.py

--- ex02.py
+++
@@ -1,9 +0,0 @@
-caracteres = input("Digite uma palavra ou frase: ").strip().upper()
-separar = caracteres.split()
-juntar = "".join(separar)
-contrario = juntar[::-1]
-
-if juntar == contrario:
-    print("A sequência de caracteres é palíndromo")
-else:
-    print("A sequência de caracteres não é palíndromo")
